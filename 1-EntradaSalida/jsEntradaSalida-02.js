595e8ec25e8c7aac5520835f92ac7a6981f738e8
Silvina Gauto <silvigauto02@gmail.com>
2023-01-30 12:26:08 -0300

    ejercicio 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,12 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	//alert("ok");
+	let nombre ;
+	nombre = prompt ('Ingrese un nombre')
+
+	//nombre = 'Mario'; //operador asignacion
+
+	alert(nombre)
 }
 
